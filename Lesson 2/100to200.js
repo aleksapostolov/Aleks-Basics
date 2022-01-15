0b77323fb4da3be9374528eca88deb7f85345415
aleksapostolov <aleksandarvapostolov@gmail.com>
2022-01-15 21:40:32 +0200

    commit

---
+++ Lesson 2/100to200.js
@@ -0,0 +1,12 @@
+function numbers100To200(input){
+    const number = Number(input[0]);
+    if (number <= "99") {
+        console.log("Less than 100");
+    } else if (number >= 100 && number <= 200){
+        console.log("Between 100 and 200")
+    } else {
+        console.log("Greater than 200");
+    }
+
+}
+numbers100To200(["295"]);
