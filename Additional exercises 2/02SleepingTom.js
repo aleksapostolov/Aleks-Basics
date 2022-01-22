28e73d6c17b60346304f69cbf5a9b9d886497906
Aleks Apostolov <97387270+aleksapostolov@users.noreply.github.com>
2022-01-22 14:12:11 +0200

    commit

--- Additional exercises 2/02SleepingTom.js
+++ Additional exercises 2/02SleepingTom.js
@@ -1,21 +1,21 @@
-function sleepingTom(input){
-    const daysOff = Number(input[0]);
-    const workingDays = 365 - daysOff;
-    const playMinutesInWorkDays = workingDays * 63;
-    const playMinutesInDaysOff = daysOff * 127;
-    const playMinutesTotal = playMinutesInDaysOff + playMinutesInWorkDays;
-    const tomNorm = 30000;
+function sleepingTom(input) {
+  const daysOff = Number(input[0]);
+  const workingDays = 365 - daysOff;
+  const playMinutesInWorkDays = workingDays * 63;
+  const playMinutesInDaysOff = daysOff * 127;
+  const playMinutesTotal = playMinutesInDaysOff + playMinutesInWorkDays;
+  const tomNorm = 30000;
 
-    const remainingMinutes = Math.abs(tomNorm - playMinutesTotal);
-    const hours = Math.floor(remainingMinutes / 60) 
-    const minutes = Math.floor(remainingMinutes % 60)
+  const remainingMinutes = Math.abs(tomNorm - playMinutesTotal);
+  const hours = Math.floor(remainingMinutes / 60);
+  const minutes = Math.floor(remainingMinutes % 60);
 
-    if ( tomNorm >= playMinutesTotal) {
-        console.log("Tom sleeps well");
-        console.log(`${hours} hours and ${minutes} minutes less for play`)
-    } else {
-        console.log ("Tom will run away");
-        console.log(`${hours} hours and ${minutes} minutes more for play`);
-    }
+  if (tomNorm >= playMinutesTotal) {
+    console.log("Tom sleeps well");
+    console.log(`${hours} hours and ${minutes} minutes less for play`);
+  } else {
+    console.log("Tom will run away");
+    console.log(`${hours} hours and ${minutes} minutes more for play`);
+  }
 }
 sleepingTom(["20"]);
---
+++ Lesson 3/06NumberInRange.js
@@ -0,0 +1,10 @@
+function NumberInRange(input){
+    const num = Number(input[0]);
+
+    if (num >= -100 && num <= 100 && num !== 0){
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
+}
+NumberInRange(["70"]);
