5067c957c5cec3ed19f052bab9e852bffbff6975
Aleks Apostolov <97387270+aleksapostolov@users.noreply.github.com>
2022-02-05 11:57:24 +0200

    commit

--- Lesson 5/03SumNumbers.js
+++ Lesson 5/03SumNumbers.js
@@ -1,6 +1,6 @@
 function numbers(input) {
 
-    let index = 0;
+    /* let index = 0;
     let result = Number(input[index]);
     index++
     let number = Number(input[index]);
@@ -13,6 +13,21 @@ function numbers(input) {
         index++;
         sum += number
     }
+    console.log(sum); */
+
+    let index = 0;
+    let num = Number(input[index]);
+    index++;
+
+    let sum = 0;
+
+    while(sum < num){
+        let tempNumber = Number(input[index]);
+        index++
+
+        sum += tempNumber;
+    }
+
     console.log(sum);
 }
 numbers(["100", "10", "20", "30", "40"]);
